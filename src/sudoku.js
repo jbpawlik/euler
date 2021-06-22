a9150cdd78410323cd9f88d1b73e163078a794b2
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-22 16:25:22 -0700

    add rowIncludesNumbers function; pass test
    Co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -24,6 +24,14 @@ export default function Sudoku() {
   this.col3 = [3,2,1];
 }
 
+let sudoku = new Sudoku()
+
+sudoku.row1.every(element => sudoku.row2.includes(element))
+
+if (sudoku.row1 === sudoku.row2) {
+  return true
+}
+
 Sudoku.prototype.includesAllNumbers = function() {
   if (this.row1.includes(1) && this.row1.includes(2) && this.row1.includes(3)) {
     return true;
@@ -31,6 +39,21 @@ Sudoku.prototype.includesAllNumbers = function() {
     return false;
   }
 }
+
+
+
+Sudoku.prototype.rowIncludesNumbers = function() {
+  
+  this.row1.forEach(function(element) {
+    if (element.includes(1) && element.includes(2) && element.includes(3)) {
+    return true; 
+    } else {
+    return false;
+  }
+})
+} 
+
+sudoku.rowIncludesNumbers(this.row1)
 
 Sudoku.prototype.firstColumnTrue = function() {
   if (this.col1.includes(1) && this.col1.includes(2) && this.col2.includes(3)) {
