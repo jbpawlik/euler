46b5d45d8b3e4365ab2efa0e2ec8573030102a33
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-22 14:30:21 -0700

    add functions to find and sum multiples of five
    Co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- __tests__/multiples.test.js
+++ __tests__/multiples.test.js
@@ -1,7 +1,5 @@
 import HoldNumbers from "../src/multiples.js"
 
-
-
 describe('divide(num)', () => {
   let holdNumbers;
   
@@ -10,17 +8,28 @@ describe('divide(num)', () => {
   });
 
   test('should divide num by 3 without remainder and output answer', () => {
-    expect(holdNumbers.divide(10)).toEqual(3);
+    expect(holdNumbers.divideBy3(10)).toEqual(3);
+  });
+  
+  test('should divide num by 5 without remainder and output answer', () => {
+    expect(holdNumbers.divideBy5(10)).toEqual(2);
   });
 
   test('should loop in the array however many times numberDivided is and multiply by 3', () => {
-    expect(holdNumbers.divide(10)).toEqual(3);
+    expect(holdNumbers.divideBy3(10)).toEqual(3);
     expect(holdNumbers.multiplyBy3()).toEqual([3,6,9]);
   });
 
-  test('should add multiples of three and return sum', () => {
-    expect(holdNumbers.divide(10)).toEqual(3);
-    expect(holdNumbers.multiplyBy3()).toEqual([3,6,9]);
-    expect(holdNumbers.addMultiples()).toEqual(18);
+  test('should loop in the array however many times numberDivided is and multiply by 5', () => {
+    expect(holdNumbers.divideBy5(10)).toEqual(2);
+    expect(holdNumbers.multiplyBy5()).toEqual([5, 10]);
+  });
+
+  test('should add multiples and return sum', () => {
+    expect(holdNumbers.divideBy3(15)).toEqual(5);
+    expect(holdNumbers.multiplyBy3()).toEqual([3,6,9,12,15]);
+    expect(holdNumbers.divideBy5(15)).toEqual(3);
+    expect(holdNumbers.multiplyBy5()).toEqual([3,6,9,12,15,5,10,15]);
+    expect(holdNumbers.addMultiples()).toEqual(60);
   })
 });
